e5140dcc26c2889ce170d66c69a5989f907ec9a4
vietnq <vietnqhe151190@fpt.edu.vn>
2023-10-18 15:37:58 +0700

    fix-field- popup-add-specimens

--- DCMS_Client/src/app/pages/admin/specimens/specimens.component.ts
+++ DCMS_Client/src/app/pages/admin/specimens/specimens.component.ts
@@ -11,5 +11,22 @@ export class SpecimensComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  columns:any = {
+    raham: true,
+    tenmau: true,
+    tenbenhnhan: true,
+    chatlieu: true,
+    // Thêm các cột khác ở đây và đặt giá trị ban đầu cho mỗi cột.
+  };
+
+  showColumnSelector = false; // Biến để kiểm soát việc hiển thị radio buttons
+
+  columnList = [
+    { name: 'raham', label: 'Răng/Hàm' },
+    { name: 'tenmau', label: 'Tên mẫu' },
+    { name: 'tenbenhnhan', label: 'Tên bệnh nhân' },
+    { name: 'chatlieu', label: 'Chất liệu' },
+    // Thêm các cột khác tương tự
+  ];
 
 }
