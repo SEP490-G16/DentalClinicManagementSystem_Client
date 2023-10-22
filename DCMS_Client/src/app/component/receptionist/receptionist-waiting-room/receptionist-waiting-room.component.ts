a8fc882b2228f93b30aa71f112f6ae9db2fd7066
Phạm Thanh Long <pham.danthanhlong@gmail.com>
2023-10-22 15:04:57 +0700

    ok

--- DCMS_Client/src/app/component/receptionist/receptionist-waiting-room/receptionist-waiting-room.component.ts
+++ DCMS_Client/src/app/component/receptionist/receptionist-waiting-room/receptionist-waiting-room.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-receptionist-waiting-room',
   templateUrl: './receptionist-waiting-room.component.html',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceptionistWaitingRoomComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  preventNavigation(event: Event) {
+    event.preventDefault();
   }
 
   imageURL: string | ArrayBuffer = '';
