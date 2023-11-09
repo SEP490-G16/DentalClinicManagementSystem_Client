0839fc412d10f9b2de5bb604db870aec81dd0005
Phạm Thanh Long <pham.danthanhlong@gmail.com>
2023-11-09 11:45:00 +0700

    ui

--- DCMS_Client/src/app/component/receptionist/receptionist-timekeeping/receptionist-timekeeping.component.ts
+++ DCMS_Client/src/app/component/receptionist/receptionist-timekeeping/receptionist-timekeeping.component.ts
@@ -1,63 +1,30 @@
-import { Component } from '@angular/core';
+import { ReceptionistTimekeepingService } from './../../../service/ReceptionistService/receptionist-timekeeping.service';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgbDate, NgbCalendar, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
 import { CognitoService } from 'src/app/service/cognito.service';
 @Component({
   selector: 'app-receptionist-timekeeping',
   templateUrl: './receptionist-timekeeping.component.html',
   styleUrls: ['./receptionist-timekeeping.component.css']
 })
-export class ReceptionistTimekeepingComponent {
+export class ReceptionistTimekeepingComponent implements OnInit {
+  clockin:string = "";
+  clockout:string = "";
+  timeClockin:string = "";
+  timeClockout:string = "";
+  constructor(private cognitoService: CognitoService,
+    private timekeepingService:ReceptionistTimekeepingService,
+    private router: Router) {
+  }
+  ngOnInit(): void {
 
-  hoveredDate: NgbDate | null = null;
-
-	fromDate: NgbDate | null;
-	toDate: NgbDate | null;
-  placement = 'right';
-	constructor(private calendar: NgbCalendar, private cognitoService:CognitoService, private router:Router,public formatter: NgbDateParserFormatter) {
-		this.fromDate = calendar.getToday();
-		this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
-	}
-
-	onDateSelection(date: NgbDate) {
-		if (!this.fromDate && !this.toDate) {
-			this.fromDate = date;
-		} else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
-			this.toDate = date;
-		} else {
-			this.toDate = null;
-			this.fromDate = date;
-		}
-	}
-
-	isHovered(date: NgbDate) {
-		return (
-			this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
-		);
-	}
-
-	isInside(date: NgbDate) {
-		return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
-	}
-
-	isRange(date: NgbDate) {
-		return (
-			date.equals(this.fromDate) ||
-			(this.toDate && date.equals(this.toDate)) ||
-			this.isInside(date) ||
-			this.isHovered(date)
-		);
-	}
-
-	validateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
-		const parsed = this.formatter.parse(input);
-		return parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
-	}
-
-  signOut() {
-      this.cognitoService.signOut().then(() => {
-          this.router.navigate(['dangnhap']);
-      })
   }
 
+  onClockin() {
+
+  }
+
+  onClockout() {
+
+  }
 }
---
+++ DCMS_Client/src/app/service/ReceptionistService/receptionist-timekeeping.service.ts
@@ -0,0 +1,42 @@
+import { CognitoService } from '../cognito.service';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReceptionistTimekeepingService {
+  private apiUrl = 'https://gg1spfr4gl.execute-api.ap-southeast-1.amazonaws.com/dev';
+
+  constructor(private http: HttpClient, private cognitoService:CognitoService) { }
+
+  getTimekeeping(startTime:number, endTime:number):Observable<any> {
+    let idToken = sessionStorage.getItem("id_Token");
+
+    const headers = new HttpHeaders({
+      'Authorization': `${idToken}`,
+      'Accept':'application/json'
+    });
+      return this.http.get(`${this.apiUrl}/timekeeping/${startTime}/${endTime}`, { headers });
+  }
+
+  postTimekeeping(PostTimekeeping:any): Observable<any> {
+    let idToken = sessionStorage.getItem("id_Token");
+    const headers = new HttpHeaders({
+      'Authorization': `${idToken}`,
+      "Content-Type": "application/json; charset=utf8"
+    });
+    const requestBody = JSON.stringify(PostTimekeeping);
+    return this.http.post(`${this.apiUrl}/timekeeping `, requestBody, { headers });
+  }
+
+  deleteTimekeeping(startTime:number, endTime:number):Observable<any> {
+    let idToken = sessionStorage.getItem("id_Token");
+
+    const headers = new HttpHeaders({
+      'Authorization': `${idToken}`
+    });
+      return this.http.delete(`${this.apiUrl}/timekeeping/${startTime}/${endTime}`, { headers });
+  }
+}
