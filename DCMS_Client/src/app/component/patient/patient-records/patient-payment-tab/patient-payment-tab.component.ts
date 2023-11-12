980920cd9c8bf7254a2aaf924f070bac8f4fa05f
Phạm Thanh Long <pham.danthanhlong@gmail.com>
2023-11-12 22:34:53 +0700

    merge

--- DCMS_Client/src/app/component/patient/patient-records/patient-payment-tab/patient-payment-tab.component.ts
+++ DCMS_Client/src/app/component/patient/patient-records/patient-payment-tab/patient-payment-tab.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { PatientService } from 'src/app/service/PatientService/patient.service';
+import { CognitoService } from 'src/app/service/cognito.service';
 
 @Component({
   selector: 'app-patient-payment-tab',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./patient-payment-tab.component.css']
 })
 export class PatientPaymentTabComponent implements OnInit {
+  id: string = "";
 
-  constructor() { }
+  constructor(
+    private patientService: PatientService,
+    private route: ActivatedRoute,
+    private cognitoService: CognitoService,
+    private router: Router,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
-  }
+    this.id = this.route.snapshot.params['id'];
 
+  }
+  navigateHref(href: string) {
+    this.router.navigate(['' + href + this.id]);
+  }
 }
