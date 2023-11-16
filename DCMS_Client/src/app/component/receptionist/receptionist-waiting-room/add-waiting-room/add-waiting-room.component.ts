fb203d321f4efefe5366437d6325334d5692004f
TvThe <tranvanthe10101@gmail.com>
2023-11-16 09:39:18 +0700

    waiting room

--- DCMS_Client/src/app/component/receptionist/receptionist-waiting-room/add-waiting-room/add-waiting-room.component.ts
+++ DCMS_Client/src/app/component/receptionist/receptionist-waiting-room/add-waiting-room/add-waiting-room.component.ts
@@ -15,8 +15,8 @@ import { MedicalProcedureGroupService } from 'src/app/service/MedicalProcedureSe
 export class AddWaitingRoomComponent implements OnInit {
 
   POST_WAITTINGROOM: IPostWaitingRoom;
-
-  //
+  phone_suggest: string = '';
+  name_suggest: string = '';
   phone_number: string = '';
   constructor(
     private WaitingRoomService: ReceptionistWaitingRoomService,
@@ -24,12 +24,12 @@ export class AddWaitingRoomComponent implements OnInit {
     private renderer: Renderer2,
     private toastr: ToastrService,
     private router: Router,
-    private medicaoProcedureGroupService:MedicalProcedureGroupService
+    private medicaoProcedureGroupService: MedicalProcedureGroupService
   ) {
 
     this.POST_WAITTINGROOM = {
       epoch: 0,
-      produce_id: "0",
+      produce_id: '',
       produce_name: '',
       patient_id: '',
       patient_name: '',
@@ -44,23 +44,33 @@ export class AddWaitingRoomComponent implements OnInit {
     status: '',
     reason: ''
   }
-  listGroupService : any[] = [];
+  listGroupService: any[] = [];
   isSubmitted: boolean = false;
   ngOnInit(): void {
-    
+    const patientData = localStorage.getItem("patient");
+    if (patientData === null) {
+      console.log("Dữ liệu không tồn tại trong Local Storage.");
+    } else {
+      const dataOfLocale = JSON.parse(patientData);
+      this.phone_suggest = dataOfLocale.phone_number;
+      this.name_suggest = dataOfLocale.patient_name;
+    }
     this.getListGroupService();
   }
 
 
   onPhoneInput() {
-    // console.log();
+    if (this.phone_number.length !== 10) {
+      return;
+    }
+
     this.resetValidate();
     if (!this.phone_number) {
       this.validateWatingRoom.phone = "Vui lòng nhập số điện thoại!";
       this.isSubmitted = true;
     }
     else if (!this.isVietnamesePhoneNumber(this.phone_number)) {
-      this.validateWatingRoom.phone = "Số điện thoại không hợp lệ"
+      this.validateWatingRoom.phone = "Số điện thoại không hợp lệ";
     }
     else {
       this.PATIENT_SERVICE.getPatientPhoneNumber(this.phone_number).subscribe((data) => {
@@ -70,29 +80,28 @@ export class AddWaitingRoomComponent implements OnInit {
         (err) => {
           this.showErrorToast("Không tìm thấy số điện thoại");
         }
-      )
+      );
     }
-
   }
   onProcedureChange(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     const selectedValue = selectElement.value;
-  
+
     // Logic to get the name from the selected ID
     const selectedProcedure = this.listGroupService.find(
       (procedure) => procedure.medical_procedure_group_id === selectedValue
     );
-  
+
     if (selectedProcedure) {
       this.POST_WAITTINGROOM.produce_id = selectedProcedure.medical_procedure_group_id;
       this.POST_WAITTINGROOM.produce_name = selectedProcedure.name;
     }
   }
-  
+
   getListGroupService() {
     this.medicaoProcedureGroupService.getMedicalProcedureGroupList().subscribe((res: any) => {
       this.listGroupService = res.data;
-      
+
       if (this.listGroupService.length > 0) {
         this.POST_WAITTINGROOM.produce_id = this.listGroupService[0].medical_procedure_group_id;
         this.POST_WAITTINGROOM.produce_name = this.listGroupService[0].name; // Thêm dòng này
@@ -100,9 +109,14 @@ export class AddWaitingRoomComponent implements OnInit {
     });
   }
   onPostWaitingRoom() {
-    //Convert date to timestamp:
+    const storedPatientIdsString = localStorage.getItem('listPatientId');
+    let storedPatientIds = [];
+
+    if (storedPatientIdsString) {
+      storedPatientIds = JSON.parse(storedPatientIdsString);
+    }
     const currentDateTimeGMT7 = moment().tz('Asia/Ho_Chi_Minh');
-      this.POST_WAITTINGROOM.epoch = Math.floor(currentDateTimeGMT7.valueOf()/1000);
+    this.POST_WAITTINGROOM.epoch = Math.floor(currentDateTimeGMT7.valueOf() / 1000);
     this.resetValidate();
     if (!this.phone_number) {
       this.validateWatingRoom.phone = "Vui lòng nhập số điện thoại!";
@@ -116,11 +130,11 @@ export class AddWaitingRoomComponent implements OnInit {
       this.validateWatingRoom.procedure = "Vui lòng chọn loại điều trị!";
       this.isSubmitted = true;
     }
-    if (!this.POST_WAITTINGROOM.reason) {
-      this.validateWatingRoom.reason = "Vui lòng nhập lý do khám!";
-      this.isSubmitted = true;
-    }
     if (this.isSubmitted) {
+      return;
+    }
+    if (storedPatientIds.includes(this.POST_WAITTINGROOM.patient_id)) {
+      this.showErrorToast('Bệnh nhân đã tồn tại trong phòng chờ!');
       return;
     }
     console.log("Hehe: ", this.POST_WAITTINGROOM);
@@ -131,7 +145,7 @@ export class AddWaitingRoomComponent implements OnInit {
         this.phone_number = '';
         this.POST_WAITTINGROOM = {
           epoch: 0,
-          produce_id: "1",
+          produce_id: '',
           produce_name: '',
           patient_id: '',
           patient_name: '',
@@ -169,8 +183,8 @@ export class AddWaitingRoomComponent implements OnInit {
   close() {
     this.POST_WAITTINGROOM = {
       epoch: 0,
-      produce_id: "1",
-      produce_name: "1",
+      produce_id: '',
+      produce_name: '',
       patient_id: '',
       patient_name: '',
       reason: '',
