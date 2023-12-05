c222c4f2792d7317c97adda354c2a8baad3169ea
Phạm Thanh Long <144437972+pham-thanh-long@users.noreply.github.com>
2023-12-05 10:54:03 +0700

    Lịch hẹn

--- DCMS_Client/src/app/component/receptionist/receptionist-appointment-list/receptionist-appointment-list.component.ts
+++ DCMS_Client/src/app/component/receptionist/receptionist-appointment-list/receptionist-appointment-list.component.ts
@@ -73,6 +73,7 @@ export class ReceptionistAppointmentListComponent implements OnInit {
   searchText: string = '';
   filteredAppointments: any;
   appointmentList: RootObject[] = [];
+
   listGroupService: any[] = [];
 
   startDate: any;
@@ -284,6 +285,11 @@ export class ReceptionistAppointmentListComponent implements OnInit {
     }
   }
 
+  onNewAppointmentAdded(newAppointment: any) {
+    console.log(newAppointment);
+    this.filteredAppointments = newAppointment;
+  }
+
   selectedAppointment: ISelectedAppointment;
   dateString: any;
   timeString: any;
@@ -300,6 +306,7 @@ export class ReceptionistAppointmentListComponent implements OnInit {
 
   deleteAppointment(appointment: any, dateTimestamp: any, event: Event) {
     event.stopPropagation();
+    console.log(appointment);
     const modalRef = this.modalService.open(ConfirmDeleteModalComponent);
     modalRef.componentInstance.message = `Bạn có chắc chắn muốn xóa lịch hẹn của bệnh nhân ${appointment.patient_name} không?`;
     modalRef.result.then((result) => {
@@ -322,7 +329,15 @@ export class ReceptionistAppointmentListComponent implements OnInit {
         this.appointmentService.deleteAppointment(dateTimestamp, appointment.appointment_id).subscribe(response => {
           console.log("Xóa thành công");
           this.showSuccessToast('Xóa lịch hẹn thành công!');
-          window.location.reload();
+          this.filteredAppointments = this.filteredAppointments.map((app: any) => ({
+            ...app,
+            appointments: app.appointments.map((ap: any) => ({
+              ...ap,
+              details: ap.details.filter((detail: any) => detail.appointment_id !== appointment.appointment_id)
+            })).filter((ap: any) => ap.details.length > 0)
+          })).filter((app: any) => app.appointments.length > 0);
+
+          console.log("Đã xóa: ", this.filteredAppointments);
         }, error => {
           this.showErrorToast("Lỗi khi cập nhật");
           this.showErrorToast("Lỗi khi xóa");
@@ -533,6 +548,8 @@ export class ReceptionistAppointmentListComponent implements OnInit {
   }
 
   openAddAppointmentModal() {
+    this.filteredAppointments = this.filteredAppointments;
+    console.log("Filtered Appointment: ", this.filteredAppointments);
     this.datesDisabled = this.datesDisabled;
   }
 
