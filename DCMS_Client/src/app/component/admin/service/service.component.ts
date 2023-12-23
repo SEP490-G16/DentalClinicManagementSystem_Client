63abd95b8b2022341025dcf4628a2a2d4a1390b5
Phạm Thanh Long <144437972+pham-thanh-long@users.noreply.github.com>
2023-12-23 18:40:20 +0700

    fix defect log

--- DCMS_Client/src/app/component/admin/service/service.component.ts
+++ DCMS_Client/src/app/component/admin/service/service.component.ts
@@ -8,7 +8,7 @@ import { ResponseHandler } from "../../utils/libs/ResponseHandler";
 import {
   ConfirmDeleteModalComponent
 } from "../../utils/pop-up/common/confirm-delete-modal/confirm-delete-modal.component";
-import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-service',
@@ -16,7 +16,7 @@ import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
   styleUrls: ['./service.component.css']
 })
 export class ServiceComponent implements OnInit {
-  medicalProcedureGroups: any;
+  medicalProcedureGroups: any [] = [];
   filteredMgData: any[] = [];
   medicalProcedureList: any[] = [];
   constructor(private medicalProcedureGroupService: MedicalProcedureGroupService,
@@ -87,17 +87,17 @@ export class ServiceComponent implements OnInit {
     modalRef.componentInstance.message = message;
     return modalRef.result;
   }
-  deleteMedicalProcedureGroup(id: string,mg_name:string) {
+  deleteMedicalProcedureGroup(id: string, mg_name: string) {
     this.openConfirmationModal(`Bạn có chắc chắn muốn xóa nhóm thủ thuật ${mg_name} không?`).then((result) => {
       if (result) {
         this.medicalProcedureGroupService.deleteMedicalProcedureGroup(id)
           .subscribe((res) => {
-              this.toastr.success('Xoá nhóm thủ thuật thành công !');
-              const index = this.filteredMgData.findIndex((medicalGroup: any) => medicalGroup.mg_id === id);
-                  if (index !== -1) {
-                    this.filteredMgData.splice(index, 1);
-                  }
-            },
+            this.toastr.success('Xoá nhóm thủ thuật thành công !');
+            const index = this.filteredMgData.findIndex((medicalGroup: any) => medicalGroup.mg_id === id);
+            if (index !== -1) {
+              this.filteredMgData.splice(index, 1);
+            }
+          },
             (error) => {
               //this.showErrorToast('Xóa mẫu vật thất bại');
               ResponseHandler.HANDLE_HTTP_STATUS(this.medicalProcedureGroupService.url + "/medical-procedure-group/" + id, error);
@@ -107,17 +107,24 @@ export class ServiceComponent implements OnInit {
     });
   }
 
-  deleteMedicalProcedure(id: string,mp_name:string) {
+  deleteMedicalProcedure(id: string, mp_name: string) {
+    console.log("procedure: ", this.filteredMgData);
     this.openConfirmationModal(`Bạn có chắc chắn muốn xóa thủ thuật ${mp_name} không?`).then((result) => {
       if (result) {
         this.medicalProcedure.deleteMedicalProcedure(id)
           .subscribe((res) => {
-              this.toastr.success('Xoá thủ thuật thành công !');
-              const index = this.medicalProcedureList.findIndex((medicalG: any) => medicalG.mp_id === id);
-                  if (index !== -1) {
-                    this.medicalProcedureList.splice(index, 1);
-                  }
-            },
+            this.toastr.success('Xoá thủ thuật thành công !');
+            const index = this.medicalProcedureList.findIndex((medicalG: any) => medicalG.mp_id === id);
+            if (index !== -1) {
+              this.medicalProcedureList.splice(index, 1);
+            }
+
+            const index2 = this.medicalProcedureGroups.findIndex((medicalG: any) => medicalG.mp_id === id);
+            if (index2 !== -1) {
+              this.medicalProcedureGroups.splice(index2, 1);
+            }
+
+          },
             (error) => {
               //this.showErrorToast('Xóa mẫu vật thất bại');
               ResponseHandler.HANDLE_HTTP_STATUS(this.medicalProcedure.url + "/medical-procedure/" + id, error);
